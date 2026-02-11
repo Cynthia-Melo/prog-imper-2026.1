d7ac19c0a625c08cf552946960aba4e37c224f8b
Cynthia Melo <cynthia.00000860095@unicap.br>
2026-02-11 00:24:20 +0000

    Aula 01 -Criação do programa olá turma.

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Exercicio01 {
+    public static Scanner imput = new Scanner ( System.in);
     public static void main ( String [] args){
         System.out.println ("Olá, Turma!");
     }
+    int idade;
 }
